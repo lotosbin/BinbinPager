fe33285f00a8fe54c0c4c50a7c4b3f7d72436d5a
liubinbin <lotosbin@gmail.com>
2015-12-23 10:38:50 +0800

    .

--- BinbinPagerIntergrationTopData/Class1.cs
+++ BinbinPagerIntergrationTopData/Class1.cs
@@ -4,9 +4,9 @@ using Top.Data;
 
 namespace BinbinPagerIntergrationTopData {
     public static class Class1 {
-        public static void ToPagedList(this SqlBuilder src, Pager1 pager) {
-            var pagedList = src.ToPagedList(pager.Skip, pager.PageSize);
-
+        public static BinbinPager.PagedList<T> ToPagedList<T>(this SqlBuilder src, Pager1 pager) {
+            var pagedList = src.ToPagedList(pager.Skip, pager.PageSize).Result;
+            return new BinbinPager.PagedList<T>(pagedList.Items, new Pager1());
         }
     }
 }
