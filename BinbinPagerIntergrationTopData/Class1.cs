db8a201e7a0ffaddff4af54fd635e84aa3feeadf
liubinbin <lotosbin@gmail.com>
2015-12-23 10:36:06 +0800

    add project  BinbinPagerIntergrationTopData.csproj

---
+++ BinbinPagerIntergrationTopData/Class1.cs
@@ -0,0 +1,12 @@
+﻿using BinbinPager;
+using Top.Core;
+using Top.Data;
+
+namespace BinbinPagerIntergrationTopData {
+    public static class Class1 {
+        public static void ToPagedList(this SqlBuilder src, Pager1 pager) {
+            var pagedList = src.ToPagedList(pager.Skip, pager.PageSize);
+
+        }
+    }
+}
